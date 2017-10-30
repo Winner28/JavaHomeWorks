210f187edf0f04ffcf702abc4c66a6aaaca9822a
vladey <bagdeyv@gmail.com>
2017-10-30 22:51:16 +0300

    m6 t1 added

--- Modules/src/main/java/Main.java
+++ Modules/src/main/java/Main.java
@@ -4,12 +4,16 @@ import Module_4_IO.Task_wFour.Hollywood.PictureCreator;
 import Module_4_IO.Task_wFour.Serialize;
 import Module_5_Exceptions.Task_One.FileSystem;
 import Module_5_Exceptions.Task_Two.PropertyManager;
+import Module_6_Collections.Task_One.PropertyMap;
+
+
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public interface Main {
 
-     static void main(String[] args) throws Throwable {
-         PropertyManager propertyManager = new PropertyManager();
-         propertyManager.showPropFile("file1.properties");
-         System.out.println(propertyManager.showValueByKey("file1.properties", "1"));
-    }
+
 }
---
+++ Modules/src/main/java/Module_6_Collections/Task_One/PropertyMap.java
@@ -0,0 +1,52 @@
+package Module_6_Collections.Task_One;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class PropertyMap {
+
+    private HashMap<String, String> propertyMap;
+    private Properties properties;
+
+
+    public PropertyMap() {
+        propertyMap = new HashMap<>();
+        properties = new Properties();
+    }
+
+
+
+    private boolean propFileExists(String nameOfFile) {
+        try {
+            String realName = "./src/main/resources/" + nameOfFile.trim() + ".properties";
+            FileInputStream fileInputStream = new FileInputStream(realName);
+            properties.load(fileInputStream);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found");
+        } catch (IOException e) {
+            throw new RuntimeException("Properties failed");
+        }
+
+
+        return true;
+    }
+
+
+
+    public HashMap<?, ?> writeInMap(String propName) {
+        propFileExists(propName);
+
+        Set set = properties.keySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            String key = String.valueOf(iterator.next());
+            propertyMap.put(key, properties.getProperty(key));
+        }
+
+        return propertyMap;
+    }
+
+}
